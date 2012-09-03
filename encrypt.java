f09e08b370fdc1e9172569d9e59144dca6ee5f3a
Andrew McPherson <andrewmcp333@gmail.com>
2012-09-03 05:59:45 -0400

    Encrypts the message using the parameter of three.
    I had to define a variable for the parameter, and just for now, I
    initiated it with the value of three. Using this parameter, we loop
    through the message, raising each and every character in value. If the
    character in question becomes larger than the alphabet, we loop it around.
    
    I had forgotten to uppercase the message before encryption, so much of the
    encryption was messed up, since all of my substitutions were meant to
    operate only on uppercase letters.

--- encrypt.java
+++ encrypt.java
@@ -4,13 +4,21 @@ public class encrypt
 {
 	public static void main(String[] args)
 	{
-		String message = "Simplicity is the prerequisite for reliability";
+		int parameter = 3;
+		String message = "Simplicity is the prerequisite for reliability".toUpperCase();
 		
 		for(int aee = 0; aee < message.length(); aee++)
 		{
 			int charnum = message.charAt(aee);
 			
 			if(charnum == ' ') {charnum = '#';}
+			
+			else
+			{
+				charnum += parameter;
+				
+				if(charnum > 90) {charnum = 64 + charnum - 90;}
+			}
 			
 			System.out.print((char)charnum);
 		}
