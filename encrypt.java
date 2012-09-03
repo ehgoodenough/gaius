0900e2b8383abd7b697b30d5ce83a6f440892766
Andrew McPherson <andrewmcp333@gmail.com>
2012-09-03 05:47:42 -0400

    Iterates through each character in the message.
    I am working with the characters as integers, since we'll soon be performing
    numeric operations on each and every character as we iterate through the
    message.

--- encrypt.java
+++ encrypt.java
@@ -5,5 +5,11 @@ public class encrypt
 	public static void main(String[] args)
 	{
 		String message = "Simplicity is the prerequisite for reliability";
+		
+		for(int aee = 0; aee < message.length(); aee++)
+		{
+			int charnum = message.charAt(aee);
+			System.out.print((char)charnum);
+		}
 	}
 }
