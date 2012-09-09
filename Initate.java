e63c2b3fc68c2e49509fb6f862d972335ab50bb6
Andrew McPherson <andrewmcp333@gmail.com>
2012-09-09 19:50:58 -0400

    Added a file to utilize our cryptography function.
    I intend to eventually revise this file to run some sort of userinterface
    to prompt for messages and parameters, but for now, will only call the
    functions with embedded values.

---
+++ Initate.java
@@ -0,0 +1,11 @@
+package arcym;
+
+public class Initate
+{
+	public static void main(String[] args)
+	{
+		int[] parameter = {3, 7, 11};
+		String message = "Simplicity is the prerequisite for reliability".toUpperCase();
+		System.out.println(Gaius.encrypt(message,parameter));
+	}
+}
