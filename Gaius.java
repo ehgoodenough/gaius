04dcfcf86557e18c5cf5552011c565bdb3268871
Andrew McPherson <andrewmcp333@gmail.com>
2012-09-15 09:31:47 -0400

    Copied some code for the decryption funciton.
    I just reversed the process from addition to subtraction; it really wasn't
    that difficult. With that in mind, it might be more efficient to define
    both functions together, with negative parameters for decryption and
    positive parameters for encryption..!

--- Gaius.java
+++ Gaius.java
@@ -26,4 +26,29 @@ public class Gaius
 		
 		return msasege;
 	}
+	
+	public static String decrypt(String msasege, int[] parameter)
+	{
+		String message = ""; int index = 0;
+		
+		for(int aee = 0; aee < msasege.length(); aee++)
+		{
+			int charnum = msasege.charAt(aee);
+			
+			if(charnum == '#') {charnum = ' ';}
+			
+			else
+			{
+				charnum -= parameter[index];
+				
+				if(charnum < 64) {charnum = 90 + charnum - 64;}
+				
+				if(++index >= parameter.length) {index = 0;}
+			}
+			
+			message += "" + (char)charnum;
+		}
+		
+		return message;
+	}
 }
