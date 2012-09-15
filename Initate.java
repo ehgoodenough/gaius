41117333dca07552d3652defa77cb83fd542e98e
Andrew McPherson <andrewmcp333@gmail.com>
2012-09-15 13:50:19 -0400

    I rewrote some of the iniate code for userprompts.
    And I still have yet to allow the users to specify the parameters. Until
    then, this will give me an interface to work with as I continue to
    experiment with my cryptography.

--- Initate.java
+++ Initate.java
@@ -4,17 +4,43 @@ import java.util.Scanner;
 
 public class Initate
 {
+	private static Scanner keyboard = new Scanner(System.in);
+	private static int[] parameter = {3};
 	
 	public static void main(String[] args)
 	{
-		Scanner keyboard = new Scanner(System.in);
-		
-		int[] parameter = {3};
-		
-		System.out.print("MESSAGE :: ");
+		while(true)
+		{
+			System.out.println("Welcome to GaiusEncryption!");
+			System.out.println("Please select an option:");
+			System.out.println("[1] Encrypt a message");
+			System.out.println("[2] Decrypt a message");
+			System.out.print("OPTION#");
+			
+			int option = keyboard.nextLine().charAt(0) - 48;
+			
+			System.out.println("");
+			
+			if(option == 1) {initiateEncrypt();}
+			else if(option == 2) {initiateDecrypt();}
+		}
+	}
+	
+	private static void initiateEncrypt()
+	{
+		System.out.print("Type the message you want to encrypt: ");
 		String message = keyboard.nextLine().toUpperCase();
-		
-		System.out.print("Your encrypted message is .. ");
+		System.out.print("Your encrypted message is ");
 		System.out.println(Gaius.encrypt(message,parameter));
+		System.out.println("");
+	}
+	
+	private static void initiateDecrypt()
+	{
+		System.out.print("Type the message you want to decrypt: ");
+		String message = keyboard.nextLine().toUpperCase();
+		System.out.print("Your decrypted message is ");
+		System.out.println(Gaius.decrypt(message,parameter));
+		System.out.println("");
 	}
 }
