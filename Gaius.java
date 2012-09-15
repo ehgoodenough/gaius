14d972b68c5cc0c00c8a21f00c245a28ad9809a8
Andrew McPherson <andrewmcp333@gmail.com>
2012-09-15 14:16:50 -0400

    Coded both encrypting and decrypting together.
    Both static functions now execute a private function which will encrypt or
    decrypt the message depending on the positivity of the parameters, almost
    like a wrapperclass.

--- Gaius.java
+++ Gaius.java
@@ -2,53 +2,37 @@ package arcym;
 
 public class Gaius
 {
-	public static String encrypt(String message, int[] parameter)
+	public static String encrypt(String givenMessage, int[] givenParameters)
 	{
-		String msasege = ""; int index = 0;
-		
-		for(int aee = 0; aee < message.length(); aee++)
-		{
-			int charnum = message.charAt(aee);
-			
-			if(charnum == ' ') {charnum = '#';}
-			
-			else
-			{
-				charnum += parameter[index];
-				
-				if(charnum > 90) {charnum = 64 + charnum - 90;}
-				
-				if(++index >= parameter.length) {index = 0;}
-			}
-			
-			msasege += "" + (char)charnum;
-		}
-		
-		return msasege;
+		return crypt(givenMessage, givenParameters);
 	}
 	
-	public static String decrypt(String msasege, int[] parameter)
+	public static String decrypt(String givenMessage, int[] givenParameters)
 	{
-		String message = ""; int index = 0;
-		
-		for(int aee = 0; aee < msasege.length(); aee++)
+		int[] inversedParameters = givenParameters;
+		for(int oee = 0; oee < inversedParameters.length; oee++)
+		{inversedParameters[oee] = inversedParameters[oee] * -1;}
+		return crypt(givenMessage, inversedParameters);
+	}
+	
+	private static String crypt(String givenMessage, int[] givenParameters)
+	{
+		String processedMessage = ""; int currentParameters = 0;
+		for(int aee = 0; aee < givenMessage.length(); aee++)
 		{
-			int charnum = msasege.charAt(aee);
-			
-			if(charnum == '#') {charnum = ' ';}
-			
-			else
+			int characterValue = givenMessage.charAt(aee);
+			if(characterValue == ' ') {characterValue = '#';}
+			else if(Character.isLetter(characterValue))
 			{
-				charnum -= parameter[index];
-				
-				if(charnum < 64) {charnum = 90 + charnum - 64;}
-				
-				if(++index >= parameter.length) {index = 0;}
+				characterValue += givenParameters[currentParameters];
+				if(characterValue > 90) {characterValue = 64 + characterValue - 90;}
+				if(characterValue < 65) {characterValue = 91 + characterValue - 65;}
+				if(++currentParameters >= givenParameters.length) {currentParameters = 0;}
 			}
 			
-			message += "" + (char)charnum;
+			processedMessage += Character.toString((char)characterValue);
 		}
 		
-		return message;
+		return processedMessage;
 	}
 }
